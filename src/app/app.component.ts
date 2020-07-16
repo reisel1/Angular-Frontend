950e71bb6a5ec97121a1179ef40c42f23216e5f8
reisel1 <40385128+reisel1@users.noreply.github.com>
2020-07-16 18:07:08 +0300

    Add all my files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+// import {MatTabsModule} from '@angular/material/tabs';
+import {Observable, Observer} from 'rxjs';
+
+export interface ExampleTab {
+  label: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-tasks';
+  asyncTabs: Observable<ExampleTab[]>;
+
+  constructor() {
+    this.asyncTabs = new Observable((observer: Observer<ExampleTab[]>) => {
+      setTimeout(() => {
+        observer.next([
+          {label: 'First', content: 'Content 1'},
+          {label: 'Second', content: 'Content 2'},
+          {label: 'Third', content: 'Content 3'},
+        ]);
+      }, 1000);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
 
 import { AppComponent } from './app.component';
+// import { MatTabsModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material/tabs';
+import { TasksComponent } from './tasks/tasks.component';
+import { CreateTaskComponent } from './tasks/create-task/create-task.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    CreateTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
